90231eac87decb6e7951a36a3775cf3baa91845b
etr-abrul <abrul@etrphils.com>
2025-05-30 10:05:27 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.nav-arrow.left').addEventListener('click', () => {
+  alert('Previous slide');
+});
+
+document.querySelector('.nav-arrow.right').addEventListener('click', () => {
+  alert('Next slide');
+});
